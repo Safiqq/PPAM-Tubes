e018b2d6b50ba7e5f1cba6217386c6b5e4136c91
Carissa Tabina Rianda <89327991+carissarianda@users.noreply.github.com>
2024-05-20 17:59:05 +0700

    Update detailpengingat.tsx

--- app/(tabs)/(user)/detailpengingat.tsx
+++ app/(tabs)/(user)/detailpengingat.tsx
@@ -28,6 +28,7 @@ export default function DetailPengingatScreen() {
                                   className="absolute left-1 w-30 h-30"
                                 />
                             <LexendText bold={true} className="text-xl text-right justify-end text-white py-7 ml-3 flex-1">Tambah Pengingat</LexendText>
+
                                <Image
                                    className="absolute right-7 h-5 w-5"
                                    source={require("@/assets/images/detail_white.png")}
@@ -35,13 +36,14 @@ export default function DetailPengingatScreen() {
                         </View>
                     </Link>
                 </View>
-                 <View style={{ flex: 1, gap: 12 }}>
-
+                 <View style={{ flex: 2, gap: 12 }}>
                                              <View
                                                style={{
                                                  flexDirection: "row",
                                                  alignItems: "center",
                                                  justifyContent: "space-between",
+                                                  marginHorizontal: 20,
+                                                        marginVertical : 15
                                                }}
                                              >
                                                <View>
@@ -59,6 +61,29 @@ export default function DetailPengingatScreen() {
                                                ></View>
                                              </View>
                                              </View>
+                                             <View
+                                                           style={{
+                                                             flexDirection: "row",
+                                                             alignItems: "center",
+                                                             justifyContent: "space-between",
+                                                               marginHorizontal: 20,
+                                                               marginVertical : 5
+                                                           }}
+                                                         >
+                                                           <View>
+                                                             <LexendText bold={true}>18 Apr 2024</LexendText>
+                                                             <LexendText>Dana Darurat</LexendText>
+                                                           </View>
+                                                           <View
+                                                             style={{
+                                                               width: 12,
+                                                               height: 12,
+                                                               marginHorizontal: 14,
+                                                               backgroundColor: "#D9D9D9",
+                                                               borderRadius: 24,
+                                                             }}
+                                                           ></View>
+                                                         </View>
         </ScrollView>
     </SafeAreaView>
   );
