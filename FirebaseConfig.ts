040bd4bf5fff504ddde798052a37839879ce50be
safiqq <szarifin20041@gmail.com>
2024-05-21 10:04:45 +0700

    fix: (0 , _auth.getReactNativePersistence) is not a function

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -1,8 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { initializeAuth, getAuth, getReactNativePersistence } from 'firebase/auth';
+import { initializeAuth, getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import AsyncStorage from "@react-native-async-storage/async-storage";
+//@ts-ignore
+import { getReactNativePersistence } from '@firebase/auth/dist/rn/index.js';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
